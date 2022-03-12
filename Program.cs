ceaf0ff222c095e80a61ba61cdfc9952a1486b6d
Cristiano616 <juniorcristianobr@gmail.com>
2022-03-12 15:20:09 -0300

    finalizar exercicio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("São Paulo!");
+Console.WriteLine("Praia Grande!");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+
+
